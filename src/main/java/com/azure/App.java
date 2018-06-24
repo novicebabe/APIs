d11636293c0d56a61d93ad1cc9d9f705af184d4b
Dungbn_git <iTrader.smart@gmail.com>
2018-06-24 16:46:05 -0500

    first commit

---
+++ src/main/java/com/azure/App.java
@@ -0,0 +1,12 @@
+package com.azure;
+
+import org.restlet.resource.ServerResource;
+
+public class App extends ServerResource {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
